e0860596554fea528aa1a7e4c9a3b8d10945450f
minidiscs <shanedbutler@gmail.com>
2022-06-30 14:53:39 -0400

    add clickEvent listener

--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -1,5 +1,20 @@
 import { getWalkers } from "./database.js"
 
+const walkerCityAlert = (clickEvent) => {
+    const itemClicked = clickEvent.target
+    if (itemClicked.id.startsWith("walker")) {
+        const [,walkerId] = itemClicked.id.split("--")
+
+        for (const walker of walkers) {
+            if (walker.id === parseInt(walkerId)) {
+                window.alert(`${walker.name} services ${walker.city}`)
+            }
+        }
+    }
+}
+
+document.addEventListener("click", (clickEvent) => walkerCityAlert(clickEvent))
+
 const walkers = getWalkers()
 
 
@@ -7,7 +22,7 @@ export const Walkers = () => {
     let walkerHTML = "<ul>"
 
     for (const walker of walkers) {
-        walkerHTML += `<li>${walker.name}</li>`
+        walkerHTML += `<li id="walker--${walker.id}">${walker.name}</li>`
     }
 
     walkerHTML += "</ul>"
